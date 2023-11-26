9a7c0d4e3b2a5d0c0717d0cad8df6d914b94973a
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2023-11-26 15:04:22 -0300

    update condicoes java
    atualizando as novas alterações do AppJava

--- src/Condicoes.java
+++ src/Condicoes.java
@@ -4,6 +4,7 @@ public class Condicoes {
         System.out.println("Teste da condição que vai ser desenvolvida em Java");
         System.out.println("");
         System.out.println("Aqui começa a linhagg qued vai ser feitas as condições");
+        System.out.println("");
         System.out.println("Continuar aqui o codigo em java para estudar mais sobre a linguagem de programação java");
         System.out.println("");
         System.out.println("");
@@ -11,8 +12,22 @@ public class Condicoes {
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
 
+        //esse sout vai pular a linha e garantir um espaçamento entre o codigo
+        System.out.println("");
 
+        if (anoDeLancamento > 2022){
+            System.out.println("Lançamento que os clientes estão gostando");
+        }else{
+            System.out.println("Filme antigo que vale a pena assistir nos dias de hoje");
+        }
+
+        if (incluidoNoPlano == true && tipoPlano.equals("plus")){
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve realizar o pagamento da locação");
+        }
 
 
     }
