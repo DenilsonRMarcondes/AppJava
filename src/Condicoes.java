7179d2c88a94bedd0a4b4b6e7a8e6dfbc569fe1a
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2023-11-26 14:11:50 -0300

    update condicoes java

--- src/Condicoes.java
+++ src/Condicoes.java
@@ -3,8 +3,17 @@ public class Condicoes {
         //Variaveis para usar
         System.out.println("Teste da condição que vai ser desenvolvida em Java");
         System.out.println("");
-        System.out.println("Aqui começa a linha que vai ser feitas as condições");
+        System.out.println("Aqui começa a linhagg qued vai ser feitas as condições");
         System.out.println("Continuar aqui o codigo em java para estudar mais sobre a linguagem de programação java");
+        System.out.println("");
+        System.out.println("");
+
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+
+
 
     }
 }
