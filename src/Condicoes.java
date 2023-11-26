a709f5e5f9b08297d778acfd82afc92a711a6b52
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2023-11-26 14:11:50 -0300

    update condicoes java

--- src/Condicoes.java
+++ src/Condicoes.java
@@ -3,7 +3,7 @@ public class Condicoes {
         //Variaveis para usar
         System.out.println("Teste da condição que vai ser desenvolvida em Java");
         System.out.println("");
-        System.out.println("Aqui começa a linha que vai ser feitas as condições");
+        System.out.println("Aqui começa a linhagg qued vai ser feitas as condições");
         System.out.println("Continuar aqui o codigo em java para estudar mais sobre a linguagem de programação java");
 
     }
