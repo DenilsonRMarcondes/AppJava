658f53726e562da7f380fbc609265f73efaf0449
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2024-05-16 13:52:01 -0300

    create AppJava

---
+++ src/AppJava.java
@@ -0,0 +1,13 @@
+public class AppJava {
+    public static void main(String[] args) {
+        System.out.println("******* Aqui vai ser desenvolvido um sistema em Java *******\n");
+//        variaveis para o sistema
+        double numero = 0;
+        double numero2 = 0;
+        System.out.println("-----------");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Escreva um numero para o sistema: ");
+            System.out.println(numero2);
+        }
+    }
+}
