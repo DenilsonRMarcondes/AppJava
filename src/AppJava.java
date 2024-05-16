d37e245f545f07587710fbfeecf1ce511d184a99
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2024-05-16 13:58:03 -0300

    Update AppJava

--- src/AppJava.java
+++ src/AppJava.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class AppJava {
     public static void main(String[] args) {
         System.out.println("******* Aqui vai ser desenvolvido um sistema em Java *******\n");
 //        variaveis para o sistema
-        double numero = 0;
-        double numero2 = 0;
+        double numero = 2;
+        double numero2 = 4;
+        Scanner leitura = new Scanner(System.in);
         System.out.println("-----------");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 1; i++) {
             System.out.println("Escreva um numero para o sistema: ");
-            System.out.println(numero2);
+            System.out.println(numero2 + numero);
         }
     }
 }
