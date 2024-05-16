2587bca4ce568a3e2bb51203877961f19e67acbd
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2024-05-16 15:44:20 -0300

    Update Java

--- src/AppJava.java
+++ src/AppJava.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 public class AppJava {
     public static void main(String[] args) {
         System.out.println("******* Aqui vai ser desenvolvido um sistema em Java *******\n");
+
 //        variaveis para o sistema
         double numero = 2;
         double numero2 = 4;
         int number1 = 2;
         Scanner leitura = new Scanner(System.in);
         System.out.println("-----------");
+
+//        Aqui vai a condição Loop no sistema AppJava
         for (int i = 0; i < 1; i++) {
             System.out.println("Escreva um numero para o sistema: ");
             System.out.println(numero2 + numero);
             System.out.println("Abaixo vai o codigo while\n");
+
 //          aqui vai o codigo de while na AppJava
             int y = 1;
             while (y < 3) {
                 System.out.println(number1);
                 i++;
+
             }
         }
     }
