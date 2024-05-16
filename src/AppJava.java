187d4144e7834d46bff4946a5d8ca459fe746640
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2024-05-16 15:41:11 -0300

    Update AppJava

--- src/AppJava.java
+++ src/AppJava.java
@@ -6,11 +6,19 @@ public class AppJava {
 //        variaveis para o sistema
         double numero = 2;
         double numero2 = 4;
+        int number1 = 2;
         Scanner leitura = new Scanner(System.in);
         System.out.println("-----------");
         for (int i = 0; i < 1; i++) {
             System.out.println("Escreva um numero para o sistema: ");
             System.out.println(numero2 + numero);
+            System.out.println("Abaixo vai o codigo while\n");
+//          aqui vai o codigo de while na AppJava
+            int y = 1;
+            while (y < 3) {
+                System.out.println(number1);
+                i++;
+            }
         }
     }
 }
