4f16cfc04696e03edd2b949ed502c4f3c4cc2221
Denilson Marcondes <denirodriguesmarcondes@gmail.com>
2024-07-09 10:32:01 -0300

    update java
    update java

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # AppJava
  Código desenvolvido em java
 Esse projeto em java foi criado para estudar mais a linguagem java
+
--- src/Teste2.java
+++ src/Teste2.java
@@ -1,5 +1,7 @@
 public class Teste2 {
     public static void main(String[] args) {
         System.out.println("teste de uma nova classe criada em javaaa");
+        System.out.println("teste de uma nova classe criada em javaaa");
+        System.out.println("teste de uma nova classe criada em javaaa");
     }
 }
